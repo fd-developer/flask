53c5d095fd4569275916fa4376f12bb415b30be3
Frederic DESCHAMPS <fd-mail@laposte.net>
2020-06-27 19:04:28 +0200

    Config pour Heroku

--- config.py
+++ config.py
@@ -2,7 +2,14 @@ import os
 
 SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
 
-FB_APP_ID = 278653803499527
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FB_APP_ID = 278653803499527
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = 278653803499527 # mettre une nouvelle appli FB pour la prod
